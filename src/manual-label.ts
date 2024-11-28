a65dfacb86c14474d29f75f0b410b89b1ce7b325
Bit D13 <bit@d13.co>
2024-11-28 03:35:46 +0100

    manual label

--- src/manual-label.ts
+++ src/manual-label.ts
@@ -7,26 +7,13 @@ import { label, labelerServer } from './label.js';
 import logger from './logger.js';
 import { startMetricsServer } from './metrics.js';
 
-labelerServer.app.listen({ port: PORT, host: HOST }, (error, address) => {
-  if (error) {
-    logger.error('Error starting server: %s', error);
-  } else {
-    logger.info(`Labeler server listening on ${address}`);
-  }
-});
+// labelerServer.app.listen({ port: PORT, host: HOST }, (error, address) => {
+//   if (error) {
+//     logger.error('Error starting server: %s', error);
+//   } else {
+//     logger.info(`Labeler server listening on ${address}`);
+//   }
+// });
 
-function shutdown() {
-  try {
-    logger.info('Shutting down gracefully...');
-    labelerServer.stop();
-  } catch (error) {
-    logger.error(`Error shutting down gracefully: ${error}`);
-    process.exit(1);
-  }
-}
-
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
-
-await label('did:plc:a3hf5fssbzaoeidajyrzgz45', 'scam');
-await label('did:plc:a3hf5fssbzaoeidajyrzgz45', 'impersonator');
+await label('did:plc:l4z3o3wf5xwazpkgt3xpxaq4', 'scam');
+await label('did:plc:l4z3o3wf5xwazpkgt3xpxaq4', 'impersonator');
